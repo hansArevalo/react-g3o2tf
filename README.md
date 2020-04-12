de5b47c11725b7ad8c9bbbf1abcec714e026ba0f
hansArevalo <hans.arevalo@uptc.edu.co>
2020-04-12 00:01:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-g3o2tf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-g3o2tf)
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from '';
+const App = () => {
+
+  return <h1> hola mundo </h1>
+}
+
+render (<App />, document.getElementById('root'));
